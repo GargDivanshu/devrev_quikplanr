3805eee9c4996c7ac7ed01dd22b6c3260ce7ce55
GargDivanshu <divanshu.garg113@gmail.com>
2024-02-24 00:20:53 +0530

    env.js changes

--- src/env.js
+++ src/env.js
@@ -50,7 +50,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     GOOGLE_AI_API: process.env.GOOGLE_AI_API,
